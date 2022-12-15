7910eedd44b285ddd667d15bc7c20a95373abc1a
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-12-15 20:52:04 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StractHomework
+{
+    struct NewStruct
+    {
+        public int number; // haytarareluc chi kareli arjeq tal
+
+        public void Method()
+        {
+            Console.WriteLine(number);
+        }
+        //public NewStruct() // aranc arjeqi kanstruktor Strukturayum chi linum
+        //{
+
+        //}
+        public NewStruct(int x) // strukturayi bolor popoxakannerin petq e kanstruktorum arjeq tal
+        {
+            number = x;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewStruct structure; // = new NewStruct(); // ekzemplyar karelie stexcel nayev aranc new-i
+
+            structure.number = 9; // minjev ogtagorcely partadir petq e arjeq tal
+
+            structure.Method();
+
+            Console.WriteLine(structure.number);
+        }
+    }
+}
